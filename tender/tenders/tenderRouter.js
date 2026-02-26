edd689d6c060b9fb0d5e6b39c61a3964e15673a8
MOODANILCHOWHAN <anilchowhan1111@gmail.com>
2026-02-26 15:19:51 +0530

    added the test api

--- tender/tenders/tenderRouter.js
+++ tender/tenders/tenderRouter.js
@@ -4,5 +4,8 @@ import { Router } from "express";
 const router= Router();
 
 router.post("/addTender",tenderController.addTender);
-router.get("/getTender/:id",tenderController.getTender)
+router.get("/getTender/:id",tenderController.getTender);
+router.get("/test",async (req,res)=>{
+return res.status(200).json({message:'tender is working'});
+})
 export default router;
