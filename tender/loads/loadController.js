4ecea2f452b55f5ed0da0d9de97cc316153d6f78
MOODANILCHOWHAN <anilchowhan1111@gmail.com>
2026-03-10 16:42:46 +0530

    removed the transaction

--- tender/loads/loadController.js
+++ tender/loads/loadController.js
@@ -8,25 +8,25 @@ export const LoadControllers={
  addingLoad: async (req,res)=>{
     const session= await mongoose.startSession()
     try {
-        session.startTransaction();
+        // session.startTransaction();
         const { farmerName,gang,lorry,totalTons,kushi,pricePerTon,advance,
             batha,isKushiDistributed,tender } =req.body;
             console.log(req.body)
         const data=await new Load(req.body);
-        data.save({session})
+        data.save()
 
-       await Lorry.findByIdAndUpdate(lorry,{$push:{loads:data._id}},{session});
+       await Lorry.findByIdAndUpdate(lorry,{$push:{loads:data._id}});
        
-       await Gang.findByIdAndUpdate(gand, {$push : {loads:data._id}},{session});
+       await Gang.findByIdAndUpdate(gand, {$push : {loads:data._id}});
        
-       await Tender.findByIdAndUpdate(tender,{$push:{loads:data._id}},{session});
+       await Tender.findByIdAndUpdate(tender,{$push:{loads:data._id}});
         
-       await session.commitTransaction();
+    //    await session.commitTransaction();
        
        return res.status(200).json({message:'load added sucessfully.'})
     } catch (error) {
         console.log(error)
-        await session.abortTransaction();
+        // await session.abortTransaction();
         return res.status(500).json({message:'Something went wrong',error:error});
     }
  }
